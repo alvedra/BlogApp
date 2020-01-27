e7c01a496d9a1ec10ce53c8884bcea2e89e26816
alvedra <albertstevan98@gmail.com>
2020-01-27 22:00:39 +0700

    add login activity, design layout

---
+++ app/src/main/java/com/educate/blogapp/Activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.educate.blogapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.educate.blogapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
