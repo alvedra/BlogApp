691509c1991d79ca34602bd9963f692c7a302d96
alvedra <albertstevan98@gmail.com>
2020-01-28 13:59:02 +0700

    add login activity logic

--- app/src/main/java/com/educate/blogapp/Activities/LoginActivity.java
+++ app/src/main/java/com/educate/blogapp/Activities/LoginActivity.java
@@ -1,16 +1,110 @@
 package com.educate.blogapp.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.educate.blogapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtLoginEmail, edtLoginPass;
+    private Button loginButton;
+    private ProgressBar progressBar;
+    private TextView loginToRegister;
+
+    private FirebaseAuth firebaseAuth;
+
+    private Intent HomeActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtLoginEmail = findViewById(R.id.edtLoginEmail);
+        edtLoginPass = findViewById(R.id.edtLoginPass);
+        loginButton = findViewById(R.id.loginButton);
+        progressBar = findViewById(R.id.progressBar2);
+        loginToRegister = findViewById(R.id.tvLoginToRegister);
+
+        HomeActivity = new Intent(this, HomeActivity.class);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginButton.setVisibility(View.INVISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+
+                final String email = edtLoginEmail.getText().toString();
+                final String pass = edtLoginPass.getText().toString();
+
+                if (email.isEmpty() || pass.isEmpty()){
+                    showMessage("Please fill all fields correctly");
+                    loginButton.setVisibility(View.VISIBLE);
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    signIn(email, pass);
+                }
+            }
+        });
+
+        loginToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerActivity);
+            }
+        });
+    }
+
+    private void signIn(String email, String pass) {
+        firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    showMessage("login successful");
+                    updateUI();
+                }
+                else {
+                    showMessage("Failed to login " + task.getException().getMessage());
+                    loginButton.setVisibility(View.VISIBLE);
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+
+    private void updateUI() {
+        startActivity(HomeActivity);
+        finish();
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser != null) {
+            updateUI();
+        }
     }
 }
